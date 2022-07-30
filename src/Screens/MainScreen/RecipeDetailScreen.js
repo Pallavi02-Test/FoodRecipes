a9df1b251e7ff1c4c6b95e77e316438d4221f33a
Pallavi02-test <pallavijavalkar02@gmail.com>
2022-07-30 17:58:35 +0530

    added code for embedded video

--- src/Screens/MainScreen/RecipeDetailScreen.js
+++ src/Screens/MainScreen/RecipeDetailScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { View, Text } from 'react-native';
 import data from '../../data/db.json';
+import YoutubePlayer from 'react-native-youtube-iframe';
 
 let recipeId;
 
@@ -17,7 +18,22 @@ const RecipeScreen = ({ route }) => {
     console.log("Details : ", recipeDetails);
     return (
         <View>
-            <Text>Recipe Screen</Text>
+            <YoutubePlayer
+                height={300}
+                play={false}
+                videoId={'c-oVDb-O2Q8'}
+            />
+
+
+            <View>
+                <Text>PANEER TIKKA BINA TANDOOR - Panner
+                    Tikka is one of the most common food of choice at the
+                    restaurant and this video breaks the recipe for you, so
+                    that you can try this yummy, flavourful paneer dish at home.
+                    Try it today!
+                </Text>
+
+            </View>
         </View>
     );
 
